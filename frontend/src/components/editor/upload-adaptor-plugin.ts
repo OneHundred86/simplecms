c3429063d5a629fb0151edef63e8e93ebca4b974
lauearo <earo.lau@outlook.com>
2021-11-07 23:37:28 +0800

    [FIX] Fix upload video and image

--- frontend/src/components/editor/upload-adaptor-plugin.ts
+++ frontend/src/components/editor/upload-adaptor-plugin.ts
@@ -1,39 +1,47 @@
-import { FileUploadService } from '../../services';
-import { lastValueFrom, Subscription } from 'rxjs';
+import { FileUploadService } from "../../services";
+import { lastValueFrom, Subscription } from "rxjs";
 
 export const uploadImage = (file) => {
-    const formData = new FormData();
+  const formData = new FormData();
 
-    formData.append('file', file);
-    return lastValueFrom(FileUploadService.postImage(formData)).then((resp) => {
-        if (resp.errcode !== 0) {
-            return { default: 'https://33333.cdn.cke-cs.com/rc1DFuFpHqcR3Mah6y0e/images/a941c59ff780060b51d398434c99fe9f229615b4227f5434.png' };
-        }
-        return { default: resp.data.url };
-    }).catch(() => {
-        return { default: 'https://33333.cdn.cke-cs.com/rc1DFuFpHqcR3Mah6y0e/images/a941c59ff780060b51d398434c99fe9f229615b4227f5434.png' };
-    });
+  formData.append("file", file);
+  return Promise.resolve(
+    lastValueFrom(FileUploadService.postImage(formData)).then((resp) => {
+      if (resp.errcode !== 0) {
+        return {
+          url:
+            "https://33333.cdn.cke-cs.com/rc1DFuFpHqcR3Mah6y0e/images/a941c59ff780060b51d398434c99fe9f229615b4227f5434.png",
+        };
+      }
+      return { url: resp.data.url };
+    })
+  ).catch(() => {
+    return {
+      url:
+        "https://33333.cdn.cke-cs.com/rc1DFuFpHqcR3Mah6y0e/images/a941c59ff780060b51d398434c99fe9f229615b4227f5434.png",
+    };
+  });
 };
 
 class MyUploadAdapter {
-    private loader: any;
-    private _uploadTask: Subscription;
+  private loader: any;
+  private _uploadTask: Subscription;
 
-    constructor(loader) {
-        this.loader = loader;
-    }
+  constructor(loader) {
+    this.loader = loader;
+  }
 
-    upload = () => {
-        return this.loader.file.then(uploadImage);
-    };
+  upload = () => {
+    return this.loader.file.then(uploadImage);
+  };
 
-    abort = () => {
-        this._uploadTask.unsubscribe();
-    };
+  abort = () => {
+    this._uploadTask.unsubscribe();
+  };
 }
 
 export function UploadAdaptorPlugin(editor) {
-    editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
-        return new MyUploadAdapter(loader);
-    };
-};
+  editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+    return new MyUploadAdapter(loader);
+  };
+}
--- frontend/src/components/editor/upload-video-adaptor-plugin.ts
+++ frontend/src/components/editor/upload-video-adaptor-plugin.ts
@@ -1,39 +1,47 @@
-import { lastValueFrom, Subscription } from 'rxjs';
-import { FileUploadService } from '../../services';
+import { lastValueFrom, Subscription } from "rxjs";
+import { FileUploadService } from "../../services";
 
 export const uploadVideo = (file) => {
-    const formData = new FormData();
+  const formData = new FormData();
 
-    formData.append('file', file);
-    return lastValueFrom(FileUploadService.postVideo(formData)).then((resp) => {
-        if (resp.errcode !== 0) {
-            return { default: 'https://33333.cdn.cke-cs.com/rc1DFuFpHqcR3Mah6y0e/images/a941c59ff780060b51d398434c99fe9f229615b4227f5434.png' };
-        }
-        return { default: resp.data.url };
-    }).catch(() => {
-        return { default: 'https://33333.cdn.cke-cs.com/rc1DFuFpHqcR3Mah6y0e/images/a941c59ff780060b51d398434c99fe9f229615b4227f5434.png' };
-    });
+  formData.append("file", file);
+  return Promise.resolve(
+    lastValueFrom(FileUploadService.postVideo(formData)).then((resp) => {
+      if (resp.errcode !== 0) {
+        return {
+          url:
+            "https://33333.cdn.cke-cs.com/rc1DFuFpHqcR3Mah6y0e/images/a941c59ff780060b51d398434c99fe9f229615b4227f5434.png",
+        };
+      }
+      return { url: resp.data.url };
+    })
+  ).catch(() => {
+    return {
+      url:
+        "https://33333.cdn.cke-cs.com/rc1DFuFpHqcR3Mah6y0e/images/a941c59ff780060b51d398434c99fe9f229615b4227f5434.png",
+    };
+  });
 };
 
 class VideoUploadAdapter {
-    private loader: any;
-    private _uploadTask: Subscription;
+  private loader: any;
+  private _uploadTask: Subscription;
 
-    constructor(loader) {
-        this.loader = loader;
-    }
+  constructor(loader) {
+    this.loader = loader;
+  }
 
-    upload() {
-        return this.loader.file.then(uploadVideo);
-    }
+  upload() {
+    return this.loader.file.then(uploadVideo);
+  }
 
-    abort() {
-        return Promise.reject();
-    }
+  abort() {
+    return Promise.reject();
+  }
 }
 
 export function VideoUploadAdapterPlugin(editor) {
-    editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
-        return new VideoUploadAdapter(loader);
-    };
+  editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+    return new VideoUploadAdapter(loader);
+  };
 }
